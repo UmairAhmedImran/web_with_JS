172826db7442322fa89d76712b16d070de6eefae
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2024-05-06 07:14:26 +0500

    Adding public folder for the frontend of the application

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+    console.log("Hello Server");
+})
